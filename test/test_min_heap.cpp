6bc95e82260d864c8320c56e15d8668f11871d05
Rucha Dave <ruchadave16@gmail.com>
2023-10-26 17:45:17 -0400

    Pull stdout into python

--- test/test_min_heap.cpp
+++ test/test_min_heap.cpp
@@ -6,9 +6,9 @@
 #include <criterion/criterion.h>
 #include <criterion/new/assert.h>
 
-Test(test_min_heap, Constructor) {
-  MinHeap *new_heap = new MinHeap();
-  cr_assert((*new_heap).size == 0);
-}
+// Test(test_min_heap, Constructor) {
+//   MinHeap *new_heap = new MinHeap();
+//   cr_assert((*new_heap).size == 0);
+// }
 
 // Test(test_min_heap, )
